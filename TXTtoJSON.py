187e009473c4b3d358b8f9343308e5065c8eb86e
jjaubele <jjaubele@uc.cl>
2025-05-30 23:45:44 -0400

    tables update

--- TXTtoJSON.py
+++ TXTtoJSON.py
@@ -1,35 +1,37 @@
 import json
 
-mens_sprint_I = ["50m", "55m", "60m", "100m", "200m", "200m sh"]
-mens_sprint_II = ["300m", "300m sh", "400m", "400m sh", "500m", "500m sh"]
-mens_hurdles = ["50mH", "55mH", "60mH", "110mH", "400mH"]
+mens_sprints_I = ["50m", "55m", "60m", "100m", "200m", "200m sh"]
+mens_sprints_II = ["300m", "300m sh", "400m", "400m sh", "500m", "500m sh"]
+mens_hurdles = ["50mH", "55mH", "60mH", "110mH", "300mH", "400mH"]
 mens_relays = ["4x100m", "4x200m", "4x200m sh", "4x400m", "4x400m sh", "4x400mix", "4x400mix sh"]
 mens_middle_distance_I = ["600m", "600m sh", "800m", "800m sh", "1000m", "1000m sh"]
 mens_middle_distance_II = ["1500m", "1500m sh", "Mile", "Mile sh", "2000m", "2000m sh", "2000m SC", "3000m SC"]
 mens_long_distance = ["3000m", "3000m sh", "2 Miles", "2 Miles sh", "5000m", "5000m sh", "10000m"]
 mens_road_running_I = ["Mile",  "5 km",  "10 km", "15 km", "10 Miles", "20 km"]
 mens_road_running_II = ["HM", "25 km", "30 km", "Marathon", "100 km"]
-mens_race_walking_road = ["3kmW", "5kmW", "10kmW", "15kmW", "20kmW", "30kmW", "35kmW", "50kmW"]
+mens_race_walking_road_I = ["3km W", "5km W", "10km W", "15km W", "20km W"]
+mens_race_walking_road_II = ["HMW", "30km W", "35km W", "MarW", "50km W"]
 mens_race_walking_track_I = ["3000mW", "5000mW", "10,000mW", "15,000mW"]
 mens_race_walking_track_II = ["20,000mW", "30,000mW", "35,000mW", "50,000mW"]
 mens_jumps_throws_and_combined_events = ["HJ", "PV", "LJ", "TJ", "SP", "DT", "HT", "JT", "Hept. sh", "Dec."]
 
-womens_sprint_I = ["50m", "55m", "60m", "100m", "200m", "200m sh"]
-womens_sprint_II = ["300m", "300m sh", "400m", "400m sh", "500m", "500m sh"]
-womens_hurdles = ["50mH", "55mH", "60mH", "100mH", "400mH"]
+womens_sprints_I = ["50m", "55m", "60m", "100m", "200m", "200m sh"]
+womens_sprints_II = ["300m", "300m sh", "400m", "400m sh", "500m", "500m sh"]
+womens_hurdles = ["50mH", "55mH", "60mH", "100mH", "300mH", "400mH"]
 womens_relays = ["4x100m", "4x200m", "4x200m sh", "4x400m", "4x400m sh", "4x400mix", "4x400mix sh"]
 womens_middle_distance_I = ["600m", "600m sh", "800m", "800m sh", "1000m", "1000m sh"]
 womens_middle_distance_II = ["1500m", "1500m sh", "Mile", "Mile sh", "2000m", "2000m sh", "2000m SC", "3000m SC"]
 womens_long_distance = ["3000m", "3000m sh", "2 Miles", "2 Miles sh", "5000m", "5000m sh", "10000m"]
 womens_road_running_I = ["Mile",  "5 km",  "10 km", "15 km", "10 Miles", "20 km"]
 womens_road_running_II = ["HM", "25 km", "30 km", "Marathon", "100 km"]
-womens_race_walking_road = ["3kmW", "5kmW", "10kmW", "15kmW", "20kmW", "30kmW", "35kmW", "50kmW"]
+womens_race_walking_road_I = ["3km W", "5km W", "10km W", "15km W", "20km W"]
+womens_race_walking_road_II = ["HMW", "30km W", "35km W", "MarW", "50km W"]
 womens_race_walking_track_I = ["3000mW", "5000mW", "10,000mW", "15,000mW"]
 womens_race_walking_track_II = ["20,000mW", "30,000mW", "35,000mW", "50,000mW"]
 womens_jumps_throws_and_combined_events = ["HJ", "PV", "LJ", "TJ", "SP", "DT", "HT", "JT", "Pent. sh", "Hept."]
 
-mens_events = [mens_sprint_I, mens_sprint_II, mens_hurdles, mens_relays, mens_middle_distance_I, mens_middle_distance_II, mens_long_distance, mens_road_running_I, mens_road_running_II, mens_race_walking_road, mens_race_walking_track_I, mens_race_walking_track_II, mens_jumps_throws_and_combined_events]
-womens_events = [womens_sprint_I, womens_sprint_II, womens_hurdles, womens_relays, womens_middle_distance_I, womens_middle_distance_II, womens_long_distance, womens_road_running_I, womens_road_running_II, womens_race_walking_road, womens_race_walking_track_I, womens_race_walking_track_II, womens_jumps_throws_and_combined_events]
+mens_events = [mens_sprints_I, mens_sprints_II, mens_hurdles, mens_relays, mens_middle_distance_I, mens_middle_distance_II, mens_long_distance, mens_road_running_I, mens_road_running_II, mens_race_walking_road_I, mens_race_walking_road_II, mens_race_walking_track_I, mens_race_walking_track_II, mens_jumps_throws_and_combined_events]
+womens_events = [womens_sprints_I, womens_sprints_II, womens_hurdles, womens_relays, womens_middle_distance_I, womens_middle_distance_II, womens_long_distance, womens_road_running_I, womens_road_running_II, womens_race_walking_road_I, womens_race_walking_road_II, womens_race_walking_track_I, womens_race_walking_track_II, womens_jumps_throws_and_combined_events]
 all_events = mens_events + womens_events
 
 mens_scoring_tables = dict()
@@ -49,15 +51,29 @@ for event_family in womens_events:
         else: 
             womens_scoring_tables[event] = []
 
-first_line = 179
+first_line = 195
 lines_per_page = 50
 pages_space_lines = 2
 event_family_space_lines = 8
 pages_per_event_family = 28
-event_families = 13
+event_families = len(mens_events)
 
 with open("tablas_extraidas/tables.txt", "r") as f:
     lines = f.readlines()
+
+# Code to detect changes in the file structure
+
+# current_line = first_line
+# for i in range(12):
+#     print(f"Event family: {all_events[i]}")
+#     for page in range(pages_per_event_family):
+#         print()
+#         print(f"Page {page + 1} of {pages_per_event_family}")
+#         print(lines[current_line:current_line + lines_per_page])
+#         current_line += lines_per_page + pages_space_lines
+#     current_line += event_family_space_lines - pages_space_lines
+
+lines.insert(16270, "\n")  # Insert a new line to avoid IndexError
 
 current_line = first_line
 current_page = 0
